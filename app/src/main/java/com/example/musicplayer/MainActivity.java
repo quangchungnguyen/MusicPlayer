e6c6f1d1537a92f597c13ea34d3c61c37665c01d
quangchungnguyen <quangchungnguyen@gmail.com>
2020-06-17 16:41:16 +0700

    17/6/2020 afternoon

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,6 +18,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (mediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mediaPlayer = null;
+        }
+
         mediaPlayer = MediaPlayer.create(this, R.raw.nhac);
         Button btnPlay = (Button) this.findViewById(R.id.playBtn);
         Button btnPause = (Button) this.findViewById(R.id.pauseBtn);
@@ -28,6 +40,29 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mediaPlayer.start();
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        Toast.makeText(MainActivity.this, "I'm done!", Toast.LENGTH_SHORT).show();
+                        releaseMediaPlayer();
+
+                    }
+                });
+
+            }
+
+            public void releaseMediaPlayer() {
+                // If the media player is not null, then it may be currently playing a sound.
+                if (mediaPlayer != null) {
+                    // Regardless of the current state of the media player, release its resources
+                    // because we no longer need it.
+                    mediaPlayer.release();
+
+                    // Set the media player back to null. For our code, we've decided that
+                    // setting the media player to null is an easy way to tell that the media player
+                    // is not configured to play an audio file at the moment.
+                    mediaPlayer = null;
+                }
             }
         });
 
@@ -61,6 +96,10 @@ public class MainActivity extends AppCompatActivity {
                 textviewVolume.setText("Volume " + volumeValue * 100 + " %");
             }
         });
+
+
+
+
     }
 
 }
